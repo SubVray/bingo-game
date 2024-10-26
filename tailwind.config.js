8eea98294302279709441f1c5af8a5f07e1217b8
SubVray <jim.alberto.21@gmail.com>
2024-10-25 21:48:21 -0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
+import defaultTheme from "tailwindcss/defaultTheme"
+
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {},
+			screens: {
+				"xs": "376px",
+				"xsm": "425px",
+				...defaultTheme.screens,
+				"3xl": "1650px",
+				"4xl": "1920px",
+			},
+		},
 	},
 	plugins: [],
 }
