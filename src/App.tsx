3fe53a6f5dfcae9ac68cf7ffeadb702164e389a1
SubVray <jim.alberto.21@gmail.com>
2024-10-25 23:19:29 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -14,13 +14,19 @@ const App = () => {
 				</p>
 				<div>
 					<Button
-						onClick={() => navigate("")}
+						onClick={() => {
+							navigate("/quantity-of-bingo-cards")
+							localStorage.setItem("gameMode", "mi-bingo")
+						}}
 						className="bg-blue-500 border-blue-500/50 hover:ring ring-blue-500/30 hover:bg-blue-600 hover:border-blue-500"
 					>
 						Mi Carton Bingo
 					</Button>
 					<Button
-						onClick={() => navigate("quantity-of-bingo-cards")}
+						onClick={() => {
+							navigate("quantity-of-bingo-cards")
+							localStorage.setItem("gameMode", "carton-bingo")
+						}}
 						className="bg-orange-500 border-orange-500/50 hover:ring ring-orange-500/30 hover:bg-orange-600 hover:border-orange-500"
 					>
 						Carton Bingo
