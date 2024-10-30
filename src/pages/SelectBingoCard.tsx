e89a870edc7fee100dc42725a00be4076cea52c6
SubVray <jim.alberto.21@gmail.com>
2024-10-30 00:39:18 -0600

    Update SelectBingoCard.tsx

--- src/pages/SelectBingoCard.tsx
+++ src/pages/SelectBingoCard.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/Button"
+import { Section } from "@/components/Section"
 import { SelectBingoCards } from "@/components/SelectBingoCards"
 import { modeGames } from "@/constants/gameModes"
 import Layout from "@/layout/Layout"
@@ -17,14 +18,15 @@ const SelectBingoCard = () => {
 		useState<number>(0)
 	const [isStartButtonDisabled, setIsStartButtonDisabled] =
 		useState<boolean>(false)
-
 	const [selectedModes, setSelectedModes] = useState<MultiValue<ModeGame>>([])
 
+	// Handle change in game modes selection
 	const handleChange = (value: MultiValue<ModeGame>) => {
 		setSelectedModes(value)
 		localStorage.setItem("modeGames", JSON.stringify(value))
 	}
 
+	// Handle game start confirmation
 	const handleStartGame = () => {
 		MySwal.fire({
 			title: "Que modos de juego quieres jugar?",
@@ -36,41 +38,66 @@ const SelectBingoCard = () => {
 					options={modeGames || selectedModes}
 					placeholder="Selecciona los modos de juego"
 					onChange={handleChange}
+					styles={{
+						control: (styles) => ({
+							...styles,
+							width: "100%",
+							backgroundColor: "#0a0a0a",
+							border: "1px solid #13151a",
+							borderRadius: "0.5rem",
+							paddingTop: "0.5rem",
+							paddingBottom: "0.5rem",
+						}),
+
+						multiValue: (styles) => ({
+							...styles,
+							backgroundColor: "#13151a",
+						}),
+						multiValueLabel: (styles) => ({
+							...styles,
+							color: "#f9fafb",
+							paddingTop: "0.5rem",
+							paddingBottom: "0.5rem",
+						}),
+					}}
 				/>
 			),
 			confirmButtonColor: "#3085d6",
 			confirmButtonText: "empezar",
 			customClass: {
-				title: "text-center text-xl font-semibold max-w-[30ch] mx-auto",
+				title:
+					"text-center text-xl font-semibold max-w-[30ch] mx-auto text-neutral-200",
 				container: "w-full h-full m-0 ",
 				htmlContainer: "w-full h-[calc(100dvh-300px)] p-0  ",
+				popup: " bg-[#13151a] ",
+				input: "!bg-transparent",
 			},
 		}).then((value) => {
-			if (value.isConfirmed) {
-				navigate("/play-bingo")
-			}
+			if (value.isConfirmed) navigate("/play-bingo")
 		})
 	}
 
+	// Load default game modes on mount
 	useEffect(() => {
 		const defaultModes = [modeGames[0]]
 		setSelectedModes(defaultModes)
 		localStorage.setItem("modeGames", JSON.stringify(defaultModes))
 	}, [])
 
+	// Fetch stored number of bingo cards on mount
 	useEffect(() => {
 		const storedNumberOfBingoCards = localStorage.getItem(
 			"quantityOfBingoCards"
 		)
 
-		if (storedNumberOfBingoCards) {
-			setStoredNumberOfBingoCards(Number(storedNumberOfBingoCards))
-		}
+		setStoredNumberOfBingoCards(
+			storedNumberOfBingoCards ? Number(storedNumberOfBingoCards) : 0
+		)
 	}, [])
 
 	return (
 		<Layout>
-			<section className="w-full mt-4">
+			<Section className="w-full mt-4">
 				<header className="flex gap-4">
 					<Button
 						disabled={isStartButtonDisabled}
@@ -90,7 +117,7 @@ const SelectBingoCard = () => {
 					storedNumberOfBingoCards={storedNumberOfBingoCards}
 					setIsStartButtonDisabled={setIsStartButtonDisabled}
 				/>
-			</section>
+			</Section>
 		</Layout>
 	)
 }
