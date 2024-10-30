161335d7eb1294299a1127859c2c18873ca33c82
SubVray <jim.alberto.21@gmail.com>
2024-10-30 00:39:12 -0600

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import "@/styles/font.css"
 import "@/styles/index.css"
+// Supports weights 300-900
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
