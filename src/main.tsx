6bd549e9f3084737736dc26e3436bd2eef9a46a4
SubVray <jim.alberto.21@gmail.com>
2024-10-31 16:31:28 -0600

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,38 +3,10 @@ import "@/styles/index.css"
 // Supports weights 300-900
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
-import App from "./App.tsx"
-import PlayBingo from "./pages/PlayBingo.tsx"
-import QuantityOfBingoCards from "./pages/QuantityOfBingoCards.tsx"
-import SelectBingoCard from "./pages/SelectBingoCard.tsx"
-import SelectMiBingoCard from "./pages/SelectMiBingoCard.tsx"
-
-const router = createBrowserRouter([
-	{
-		path: "/",
-		element: <App />,
-	},
-	{
-		path: "/quantity-of-bingo-cards",
-		element: <QuantityOfBingoCards />,
-	},
-	{
-		path: "/select-bingo-card",
-		element: <SelectBingoCard />,
-	},
-	{
-		path: "/play-bingo",
-		element: <PlayBingo />,
-	},
-	{
-		path: "/select-mi-bingo-card",
-		element: <SelectMiBingoCard />,
-	},
-])
+import App from "./pages/App.tsx"
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<RouterProvider router={router} />
+		<App />
 	</StrictMode>
 )
