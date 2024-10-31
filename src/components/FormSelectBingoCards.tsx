87a782f81e60cc6c8b5991ab11c94ffed0217245
SubVray <jim.alberto.21@gmail.com>
2024-10-31 16:31:11 -0600

    Create FormSelectBingoCards.tsx

---
+++ src/components/FormSelectBingoCards.tsx
@@ -0,0 +1,49 @@
+import { Carton } from "@/types/carton"
+import { getCardById, getNextCard } from "@/utils/cardUtils"
+import { Dispatch, SetStateAction, useState } from "react"
+
+const FormSelectBingoCards = ({
+	setPreviewedCards,
+}: {
+	setPreviewedCards: Dispatch<SetStateAction<Carton[]>>
+}) => {
+	const [selectedCardId, setSelectedCardId] = useState<number>(0)
+	// Function to search cards based on the selected card ID
+	const searchCards = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault()
+		const selectedCard = getCardById(selectedCardId) as Carton
+		const nextCard = getNextCard(selectedCard) as Carton
+		const cards = []
+
+		if (selectedCard && nextCard) {
+			cards.push(selectedCard)
+			cards.push(nextCard)
+		}
+
+		setPreviewedCards(cards)
+	}
+	return (
+		<form
+			onSubmit={searchCards}
+			className="mb-6"
+		>
+			<input
+				type="number"
+				min="1"
+				name="selectedCardId"
+				max="60"
+				required
+				placeholder="Numero del carton"
+				onChange={(e) => setSelectedCardId(Number(e.target.value))}
+				className="mb-4 w-full rounded-md border-2 border-gray-800 bg-neutral-950 p-4 text-center text-xl font-semibold text-gray-50 outline-none transition hover:border-gray-700 focus:ring-2 focus:ring-blue-500"
+			/>
+			<button
+				type="submit"
+				className="block w-full rounded-md bg-blue-500 p-4 text-center text-xl font-semibold text-white transition hover:bg-blue-600 xs:p-6"
+			>
+				Buscar Carton
+			</button>
+		</form>
+	)
+}
+export default FormSelectBingoCards
