545d394bc6360e7d90072d9c4ba72f702eedd474
SubVray <jim.alberto.21@gmail.com>
2024-10-31 16:31:25 -0600

    Create useConfirmBeforeReload .ts

---
+++ src/hooks/useConfirmBeforeReload .ts
@@ -0,0 +1,22 @@
+import { useEffect } from "react"
+
+const useConfirmBeforeReload = () => {
+	useEffect(() => {
+		const handleBeforeUnload = (event: BeforeUnloadEvent) => {
+			event.preventDefault()
+			event.returnValue = "Hsy cambios sin grabar. ¿Abandonar ahora?"
+		}
+
+		// Agrega el evento `beforeunload` para prevenir la recarga
+		window.addEventListener("beforeunload", handleBeforeUnload)
+
+		return () => {
+			// Limpia el evento al desmontar el componente
+			window.removeEventListener("beforeunload", handleBeforeUnload)
+		}
+	}, [])
+
+	return null
+}
+
+export default useConfirmBeforeReload
